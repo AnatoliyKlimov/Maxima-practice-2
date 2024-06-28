5103e2019c94b12d938aa700a5f7dca8f2077a2d
Maxim93mk <kinjashov.m@rambler.ru>
2024-06-28 23:05:05 +0300

    header - готово

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,10 +11,12 @@ function Header() {
                             <p className="sale-line-descr">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
                             <a href='#' className="sale-link">ShopNow</a>
                         </div>
-                        <select className='lang-list'>
-                            <option className='lang-list-item'>Russian</option>
-                            <option className='lang-list-item'>English</option>
-                        </select>
+                        <div className="language">
+                            <select className='lang-list'>
+                                <option className='lang-list-item'>Russian</option>
+                                <option className='lang-list-item'>English</option>
+                            </select>
+                        </div>
                     </div>
                     <div className="bottom-header">
                         <h1 className="logo">Exclusive</h1>
@@ -30,9 +32,15 @@ function Header() {
                                 <button className='search-btn'></button>
                             </div>
                             <div className="icons-block">
-                                <a href="#" className='like'></a>
-                                <a href="#" className='cart'></a>
-                                <a href="#" className='user'></a>
+                                <div className="icon like-block">
+                                    <a href="#"></a>
+                                </div>
+                                <div className="icon cart-block">
+                                    <a href="#"></a>
+                                </div>
+                                <div className="icon user-block">
+                                    <a href="#"></a>
+                                </div>
                             </div>
                         </div>
                     </div>
