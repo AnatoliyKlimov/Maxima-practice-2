333cc23733e58ee32e7c6f06590c98ebe4cade32
PAVEL KOZIN <sart6zaV@yandex.ru>
2024-07-03 00:33:58 +0300

    registartion + localstorage

---
+++ src/Slices/Slice.jsx
@@ -0,0 +1,26 @@
+// Slices/CartSlice.jsx
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  login: 0,
+  auth: [],
+  loginIn: JSON.parse(localStorage.getItem('loginIn')) || false,
+};
+
+const Slice = createSlice({
+  name: 'Info',
+  initialState,
+  reducers: {
+    addNewAuth: (state, action) => {
+      const item = [];
+      item.push(action.payload[0]);
+      item.push(action.payload[1]);
+      if (state.auth.find((element) => item[0] === element[0])) return;
+      state.auth.push(item);
+    },
+  },
+});
+
+export const { addNewAuth } = Slice.actions;
+export default Slice.reducer;
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,40 +1,94 @@
 import '../login/login.css';
 import login from './img/login.png';
-function Login() {
+import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { addNewAuth } from '../../Slices/Slice';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+function Login({ onLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [Message, setMessage] = useState(' ');
+  const { auth } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    console.log(auth);
+    e.preventDefault();
+    if (username.length >= 4 && password.length) {
+      if (auth.length !== 0) {
+        for (let i = 0; i < auth.length; i++) {
+          if (auth[i][0] === username) {
+            setMessage('Такой пользователь уже зарегистрирован');
+            break;
+          } else {
+            setMessage('Пользователь зарегистрирован');
+            dispatch(addNewAuth([username, password]));
+          }
+        }
+      } else {
+        setMessage('Пользователь зарегистрирован');
+        dispatch(addNewAuth([username, password]));
+      }
+    } else {
+      setMessage('Ошибка: Логин меньше 4х символов');
+    }
+  };
+
   return (
     <>
-      <div className="container login">
-        <div className="login_left">
-          <img src={login} alt="" />
-        </div>
-        <div className="login_right">
-          <div className="login_right_top">
-            <h2 className="login_h2">Create an account</h2>
-            <span className="login_span">Enter your details below</span>
+      <form className="forms1" onSubmit={handleSubmit}>
+        <div className="container login">
+          <div className="login_left">
+            <img src={login} alt="" />
           </div>
-          <div className="form-container">
-            <input className="input_login" type="text" placeholder="Name" />
-            <input
-              className="input_login"
-              type="text"
-              placeholder="Email or Phone Number"
-            />
-            <input
-              className="input_login"
-              type="password"
-              placeholder="Password"
-            />
-            <button className="create-account-btn">Create Account</button>
-            <button className="google-signup-btn">Sign up with Google</button>
-            <div className="login_bottom_btn">
-              <button className="have-account-btn">
-                Already have account?
+          <div className="login_right">
+            <div className="login_right_top">
+              <h2 className="login_h2">Create an account</h2>
+              <span className="login_span">Enter your details below</span>
+            </div>
+            <div className="form-container">
+              <input
+                className="input_login"
+                type="text"
+                placeholder="Name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                className="input_login"
+                type="text"
+                placeholder="Email or Phone Number"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                className="input_login"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="create-account-btn" onClick={handleSubmit}>
+                Create Account
               </button>
-              <button className="login-btn">Log in</button>
+              <button className="google-signup-btn">Sign up with Google</button>
+              <div className="login_bottom_btn">
+                <button className="have-account-btn">
+                  Already have account?
+                </button>
+                <button className="login-btn">Log in</button>
+              </div>
+              {Message === 'Пользователь зарегистрирован' ? (
+                <div className="messageTrue">{Message}</div>
+              ) : Message !== 'Пользователь зарегистрирован' ? (
+                <div className="messageFalse">{Message}</div>
+              ) : null}
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </>
   );
 }
