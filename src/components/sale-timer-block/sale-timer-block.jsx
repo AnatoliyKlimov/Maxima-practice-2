48ee75b04dfb4d3109d3d6ad4bc553061d37179e
Maxim93mk <kinjashov.m@rambler.ru>
2024-07-10 17:26:43 +0300

    Добавлен таймер на страницу MainPage - готово

--- src/components/sale-timer-block/sale-timer-block.jsx
+++ src/components/sale-timer-block/sale-timer-block.jsx
@@ -1,35 +1,163 @@
 import '../sale-timer-block/sale-timer-block.css';
+import { useState, useEffect } from 'react';
+
+
+function SaleTimerBlock(props) {
+    let timerId = null;
+    let days = 0, hours = 0, minutes = 0, seconds = 0;
+    let finalDate = new Date(2024, 6, 10, 14, 15, 0);
+
+    function getSaleTime() {
+        let currentDate = new Date();
+        let diff = finalDate - currentDate;
+        const daysView = document.querySelector('.timer-days');
+        const hoursView = document.querySelector('.timer-hours');
+        const minutesView = document.querySelector('.timer-minutes');
+        const secondsView = document.querySelector('.timer-seconds');
+
+        if (diff <= 0) {
+            clearInterval(timerId);
+        }
+
+        if (diff > 0) {
+            days = Math.floor(diff / 1000 / 60 / 60 / 24);
+
+        }
+        else {
+            days = 0;
+        }
+        if (diff > 0) {
+            hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+        }
+        else {
+            hours = 0;
+        }
+        if (diff > 0) {
+            minutes = Math.floor(diff / 1000 / 60) % 60;
+
+        }
+        else {
+            minutes = 0;
+        }
+        if (diff > 0) {
+            seconds = Math.floor(diff / 1000) % 60;
+        }
+        else {
+            seconds = 0;
+
+        }
+        if (days.toString().length < 2) {
+            days = "0" + days;
+        }
+
+        if (hours.toString().length < 2) {
+            hours = "0" + hours;
+        }
+
+        if (minutes.toString().length < 2) {
+            minutes = "0" + minutes;
+        }
+
+        if (seconds.toString().length < 2) {
+            seconds = "0" + seconds;
+        }
+
+        if (daysView != null) {
+            daysView.textContent = days;
+        }
+        if (hoursView != null) {
+            hoursView.textContent = hours;
+        }
+        if (minutesView != null) {
+            minutesView.textContent = minutes;
+        }
+        if (secondsView != null) {
+            secondsView.textContent = seconds;
+        }
+    }
 
 
 
+    //  console.log(timerId )
+
+    // let finalDate = `${days} : ${hours} : ${minutes} : ${seconds}`;
+    // let date = new Date(props.days + props.hours + props.minutes + props.seconds);
+    // console.log(date)
 
 
-function SaleTimerBlock() {
+    // const [over, setOver] = useState(false);
+    //  let[[d, h, m, s], setTime]  = useState([days, hours, minutes, seconds]);
+
+    // console.log(seconds)
+
+    // const tick =()=>{
+    //     if(over) return;
+    //     setOver(true)
+    //     if(s!=0){
+    //         setTime(s--);
+    //     }
+    //     else if(s==0){
+    //         setTime(s = 59);
+    //         setTime(m--);
+    //     }
+    //     else if(m==0){
+    //         setTime(m = 59);
+    //         setTime(h--);
+    //     }
+    //     else if(h==0){
+    //         setTime(d--);
+    //     }
+    // if (hours === 0 && minutes === 0 && seconds === 0) {
+    //     setOver(true);
+    //     console.log("1");
+    // } else if (minutes === 0 && seconds === 0) {
+    //     setTime([hours - 1,  59, 59]);
+    //     console.log("2");
+    // } else if (seconds == 0) {
+    //     setTime([hours, minutes - 1, 59]);
+    //     console.log("3");
+    // } else {
+    //     setTime([h, m, s - 1]);
+    //     console.log("4");
+    // }
+    // console.log(seconds, minutes, hours,  days)
+    // }
+
+    // useEffect(()=>{
+    //     const timerID = setInterval(() => tick(), 1000);
+    //     return () => clearInterval(timerID);
+    // })
+
+
+    getSaleTime();
+    timerId = setInterval(getSaleTime, 1000);
     return (
         <>
             <div className='sale-timer-block'>
                 <div className="timer-value-block">
                     <p className="timer-value-block-d">Days</p>
-                    <p className="timer-value-block-v">03</p>
+                    <p className="timer-value-block-v timer-days"></p>
                 </div>
                 <span className='timer-splitter'>:</span>
                 <div className="timer-value-block">
                     <p className="timer-value-block-d">Hours</p>
-                    <p className="timer-value-block-v">23</p>
+                    <p className="timer-value-block-v timer-hours"></p>
                 </div>
                 <span className='timer-splitter'>:</span>
                 <div className="timer-value-block">
                     <p className="timer-value-block-d">Minutes</p>
-                    <p className="timer-value-block-v">19</p>
+                    <p className="timer-value-block-v timer-minutes"></p>
                 </div>
                 <span className='timer-splitter'>:</span>
                 <div className="timer-value-block">
                     <p className="timer-value-block-d">Seconds</p>
-                    <p className="timer-value-block-v">56</p>
+                    <p className="timer-value-block-v timer-seconds"></p>
                 </div>
             </div>
         </>
+
     )
+
 
 
 }
