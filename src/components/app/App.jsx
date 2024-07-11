6e1ffca418222efd19c8c83864b62ad27f0b0ea1
PAVEL KOZIN <sart6zaV@yandex.ru>
2024-07-12 02:31:50 +0300

    create: checkout and cart

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,8 @@
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import Login from '../login/login';
+import Checkout from '../checkout/checkout';
+import Cart from '../cart/cart';
 import MainPage from "../../pages/main-page/main-page";
 // import NotFoundPage from "../../pages/not-found-page/not-found-page";
 
@@ -8,9 +10,11 @@ function App() {
   return (
     <>
       <Header />
-      <Login />
+      <Checkout />
+   {/* <Login /> */}
       <Footer />
-      <MainPage/>
+      {/* <MainPage/> */}
+
       {/* <NotFoundPage/> */}
     </>
   );
